a2ad52b7f5e8bdf05493496b9f0eb8fb72e48431
ps-xaf <ps-xaf@users.noreply.github.com>
2020-03-05 21:49:16 +0100

    Updated to python3 and boto3

--- trusted.advisor.report.stdout.py
+++ trusted.advisor.report.stdout.py
@@ -2,11 +2,9 @@
 '''
 Gets data from AWS Trusted Advisor and prints non-ok checks.
 '''
+import boto3
+conn = boto3.client('support', region_name='us-east-1')
 
-import json
-import sys
-import boto.support
-conn = boto.support.connect_to_region('us-east-1')
 
 class bcolors:
     UNK = '\033[94m'
@@ -14,29 +12,35 @@ class bcolors:
     ERROR = '\033[91m'
     ENDCOLOR = '\033[0m'
 
-# TODO: Add code for calculating potential savings
-# potential_monthly_savings = 0
 
-checks = conn.describe_trusted_advisor_checks('en')
+checks = conn.describe_trusted_advisor_checks(language='en')
+
 checksCount = 1
 for check in checks['checks']:
-  status = conn.describe_trusted_advisor_check_summaries([check['id']])['summaries'][0]['status']
-  if status != 'ok': # and check['category'] == 'cost_optimizing':
-    color = bcolors.WARNING if status == 'warning' else bcolors.ERROR if status == 'error' else bcolors.UNK
-    print checksCount, '\tName: ' + check['name']
-    print '\tCategory: ' + check['category']
-    print '\tID : ' + check['id']
-    print '\tStatus: ', color + conn.describe_trusted_advisor_check_summaries([check['id']])['summaries'][0]['status'] + bcolors.ENDCOLOR
-    print
-    print '\tFlagged Resources: '
-    print '\t\tSlNo.\tStatus', '\t\t' if status == 'warning' else '\t', [str(md) for md in check['metadata']]
-    print '\t\t---------------------------------------------------------'
-    flaggedResources = conn.describe_trusted_advisor_check_result(check['id'],language='en')['result']['flaggedResources']
-    flaggedResourcesCount = 1
-    for resource in flaggedResources:
-      if resource['status'] != 'ok': 
+    status = conn.describe_trusted_advisor_check_summaries(checkIds=[check['id']])['summaries'][0]['status']
+    if status != 'ok':
         color = bcolors.WARNING if status == 'warning' else bcolors.ERROR if status == 'error' else bcolors.UNK
-        print '\t\t', str(flaggedResourcesCount), '\t', color + str(resource['status']), '\t', [str(item) for item in resource['metadata']],  bcolors.ENDCOLOR
-        flaggedResourcesCount += 1
-    print
+        print(checksCount, '\tName: ' + check['name'])
+        print('\tCategory: ' + check['category'])
+        print('\tID : ' + check['id'])
+        print('\tStatus: ', color + conn.describe_trusted_advisor_check_summaries(checkIds=[check['id']])['summaries'][0]['status'] + bcolors.ENDCOLOR)
+        print()
+        print('\tFlagged Resources: ')
+        print('\t\tSlNo.\tStatus', '\t\t' if status == 'warning' else '\t', [str(md) for md in check['metadata']])
+        print('\t\t---------------------------------------------------------')
+        try:
+            flaggedResources = conn.describe_trusted_advisor_check_result(checkId=check['id'], language='en')['result']['flaggedResources']
+        except KeyError:
+            print('\t\tNo flagged resources')
+            flaggedResources = {}
+        flaggedResourcesCount = 1
+        for resource in flaggedResources:
+            if resource['status'] != 'ok':
+                color = bcolors.WARNING if status == 'warning' else bcolors.ERROR if status == 'error' else bcolors.UNK
+                try:
+                    print('\t\t', str(flaggedResourcesCount), '\t', color + str(resource['status']), '\t', [str(item) for item in resource['metadata']], bcolors.ENDCOLOR)
+                except:
+                    print('\t\t', str(flaggedResourcesCount), '\t', color + str(resource['status']), '\t')
+                flaggedResourcesCount += 1
+        print
     checksCount += 1
